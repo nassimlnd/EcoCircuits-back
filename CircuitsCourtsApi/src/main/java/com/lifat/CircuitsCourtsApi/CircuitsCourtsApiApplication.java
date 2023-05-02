2412f35a402f9c5b142e28157c5bc5a891c746d7
Nassim LOUNADI <nassimlnd37@gmail.com>
2023-05-02 14:16:38 +0200

    Added the API Spring project

---
+++ CircuitsCourtsApi/src/main/java/com/lifat/CircuitsCourtsApi/CircuitsCourtsApiApplication.java
@@ -0,0 +1,13 @@
+package com.lifat.CircuitsCourtsApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitsCourtsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitsCourtsApiApplication.class, args);
+	}
+
+}
---
+++ CircuitsCourtsApi/src/test/java/com/lifat/CircuitsCourtsApi/CircuitsCourtsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifat.CircuitsCourtsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitsCourtsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
