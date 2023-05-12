cf812ca37f34bb3f097a719c667ba60bae846ff5
Nassim LOUNADI <nassimlnd37@gmail.com>
2023-05-12 11:54:54 +0200

    Added WebSecurityConfig and UserDetailsService

--- CircuitsCourtsApi/src/main/java/com/lifat/CircuitsCourtsApi/repository/UserRepository.java
+++ CircuitsCourtsApi/src/main/java/com/lifat/CircuitsCourtsApi/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.lifat.CircuitsCourtsApi.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
     User findByToken(String Token);
 
---
+++ CircuitsCourtsApi/src/main/java/com/lifat/CircuitsCourtsApi/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.lifat.CircuitsCourtsApi.security.services;
+
+import com.lifat.CircuitsCourtsApi.model.User;
+import com.lifat.CircuitsCourtsApi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+}
